85f36f4f46ff84ece822c9a8419732656503215f
Chris <1304426365@qq.com>
2023-02-14 02:14:41 -0500

    all functions completed

--- c_hexfuncs.c
+++ c_hexfuncs.c
@@ -32,7 +32,9 @@ void hex_write_string(const char s[]){
 // hex digits.  The formatted offset is stored in sbuf, which must
 // have enough room for a string of length 8.
 void hex_format_offset(unsigned offset, char sbuf[]){
+    // convert hex from least significant digit
     int index = 7;
+    // remainder -> division operations to convert to hex
     while (offset != 0) {
         unsigned remainder = 0;
         char hex_digit;
@@ -45,16 +47,38 @@ void hex_format_offset(unsigned offset, char sbuf[]){
         sbuf[index--] = hex_digit;
         offset/=16;
     }
+    // pad leading zeros
     for (int i = 0; i <= index; i++) {
         sbuf[i] = '0';
     }
+    // add null terminator in case of any garbage in the array
     sbuf[8] = '\0';
 }
 
 // Format a byte value (in the range 0-255) as string consisting
 // of two hex digits.  The string is stored in sbuf.
 void hex_format_byte_as_hex(unsigned char byteval, char sbuf[]){
-    return;
+    // convert hex from least significant digit
+    int index = 1;
+    // remainder -> division operations to convert to hex
+    while (byteval != 0) {
+        unsigned remainder = 0;
+        char hex_digit;
+        remainder = byteval % 16;
+        if (remainder >= 10) {
+            hex_digit = 87 + remainder;
+        } else {
+            hex_digit = 48 + remainder;
+        }
+        sbuf[index--] = hex_digit;
+        byteval/=16;
+    }
+    // pad leading zeros
+    for (int i = 0; i <= index; i++) {
+        sbuf[i] = '0';
+    }
+    // add null terminator in case of any garbage in the array
+    sbuf[2] = '\0';
 }
 
 // Convert a byte value (in the range 0-255) to a printable character
@@ -62,5 +86,9 @@ void hex_format_byte_as_hex(unsigned char byteval, char sbuf[]){
 // unmodified.  If byteval is not a printable character, then the
 // ASCII code for '.' should be returned.
 char hex_to_printable(unsigned char byteval){
-    return;
+    if ((byteval >= 0 && byteval <= 31) || byteval == 127) {
+        return '.';
+    } else {
+        return byteval;
+    }
 }
