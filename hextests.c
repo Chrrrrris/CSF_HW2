9549d675610bf2f42d856ba4357733456880fdea
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 <zsong30@jhu.edu>
2023-02-17 12:07:10 -0500

    fails test

--- hextests.c
+++ hextests.c
@@ -59,6 +59,9 @@ void testFormatByteAsHex(TestObjs *objs) {
   char buf[16];
   hex_format_byte_as_hex(objs->test_data_1[0], buf);
   ASSERT(0 == strcmp(buf, "48"));
+  hex_format_byte_as_hex('I', buf);
+  printf("\nsbuf is %s\n", buf);
+  ASSERT(0 == strcmp(buf, "49"));
 }
 
 void testHexToPrintable(TestObjs *objs) {
